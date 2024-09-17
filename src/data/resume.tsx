4400c18d3cd0473650b0aa2a8256aa61d261bb96
DevLucasGodoy <lucas.moraes.godoy@gmail.com>
2024-09-17 19:54:45 -0400

    Update resume.tsx

--- src/data/resume.tsx
+++ src/data/resume.tsx
@@ -15,13 +15,15 @@ export const DATA = {
     "Javascript",
     "Typescript",
     "React",
+    "React Native",
     "Next.js",
-    "Node.js",
+    "Node",
     "Postgres",
     "MySQL",
     "AWS",
     "HTML",
     "CSS",
+    "LaTeX",
   ],
   navbar: [
     { href: "/", icon: HomeIcon, label: "Home" },
@@ -89,18 +91,6 @@ export const DATA = {
       description:
         "Implemented a custom Kubernetes controller in Go to automate the deployment of MySQL and ProxySQL custom resources in order to enable 2,000+ internal developers to instantly deploy their app databases to production. Wrote several scripts in Go to automate MySQL database failovers while maintaining master-slave replication topologies and keeping Zookeeper nodes consistent with changes.",
     },
-    // {
-    //   company: "Nvidia",
-    //   href: "https://nvidia.com/",
-    //   badges: [],
-    //   location: "Santa Clara, CA",
-    //   title: "Software Engineer",
-    //   logoUrl: "/nvidia.png",
-    //   start: "January 2020",
-    //   end: "April 2020",
-    //   description:
-    //     "Architected and wrote the entire MVP of the GeForce Now Cloud Gaming internal admin and A/B testing dashboard using React, Redux, TypeScript, and Python.",
-    // },
   ],
   education: [
     {
@@ -127,60 +117,22 @@ export const DATA = {
       start: "Fev 2019",
       end: "Jun 2019",
     },
-    // {
-    //   school: "International Baccalaureate",
-    //   href: "https://ibo.org",
-    //   degree: "IB Diploma",
-    //   logoUrl: "/ib.png",
-    //   start: "2012",
-    //   end: "2016",
-    // },
   ],
   projects: [
     {
-      title: "Chat Collect",
-      href: "https://chatcollect.com",
-      dates: "Jan 2024 - Feb 2024",
-      active: true,
-      description:
-        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
-      technologies: [
-        "Next.js",
-        "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Stripe",
-        "Shadcn UI",
-        "Magic UI",
-      ],
-      links: [
-        {
-          type: "Website",
-          href: "https://chatcollect.com",
-          icon: <Icons.globe className="size-3" />,
-        },
-      ],
-      image: "",
-      video:
-        "https://pub-83c5db439b40468498f97946200806f7.r2.dev/chat-collect.mp4",
-    },
-    {
-      title: "Magic UI",
+      title: "First Class Dev",
       href: "https://magicui.design",
-      dates: "June 2023 - Present",
+      dates: "Abr 2024 - Out 2024",
       active: true,
       description:
         "Designed, developed and sold animated UI components for developers.",
       technologies: [
+        "React",
         "Next.js",
         "Typescript",
-        "PostgreSQL",
-        "Prisma",
-        "TailwindCSS",
-        "Stripe",
-        "Shadcn UI",
+        "Tailwind CSS",
         "Magic UI",
+        "Framer Motion",
       ],
       links: [
         {
@@ -194,8 +146,32 @@ export const DATA = {
           icon: <Icons.github className="size-3" />,
         },
       ],
-      image: "",
-      video: "https://cdn.magicui.design/bento-grid.mp4",
+      image: "/firstclassdev.png",
+      video: "",
+    },
+    {
+      title: "Welcome Vistos",
+      href: "https://welcomevistos.com.br",
+      dates: "Jan 2023 - Fev 2024",
+      active: true,
+      description:
+        "With the release of the [OpenAI GPT Store](https://openai.com/blog/introducing-the-gpt-store), I decided to build a SaaS which allows users to collect email addresses from their GPT users. This is a great way to build an audience and monetize your GPT API usage.",
+      technologies: [
+        "Javascript",
+        "HTML",
+        "CSS",
+        "SCSS",
+        "PHP",
+      ],
+      links: [
+        {
+          type: "Website",
+          href: "https://welcomevistos.com.br",
+          icon: <Icons.globe className="size-3" />,
+        },
+      ],
+      image: "/welcome.png",
+      video: "",
     },
     {
       title: "llm.report",
