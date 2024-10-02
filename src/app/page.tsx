e8b365433c246665c0db523e6cbf9e8c1b72a3e4
DevLucasGodoy <lucas.moraes.godoy@gmail.com>
2024-10-01 21:15:39 -0400

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -22,7 +22,8 @@ export default function Page() {
                 delay={BLUR_FADE_DELAY}
                 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none"
                 yOffset={8}
-                text={`Olá! Sou o ${DATA.name.split(" ")[0]} 👋`}
+                // text={`Olá! Sou o ${DATA.name.split(" ")[0]} 👋`}
+                text={`Olá! Sou o ${DATA.name.split(" ")[0]}.`}
               />
               <BlurFadeText
                 className="max-w-[600px] md:text-xl"
